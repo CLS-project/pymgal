aa2a635a4b9d6397030df2fcd325d7206f0b0dfd
Patrick Janulewicz <96801116+pjanul@users.noreply.github.com>
2025-04-29 12:23:23 -0400

    Added line of sight dust

--- pymgal/dusts.py
+++ pymgal/dusts.py
@@ -104,7 +104,7 @@ class calzetti(object):
 
 
 @njit(parallel=True)   # Numba drastically speeds this up,
-def gas_masses_los(g_xbins, g_ybins, g_depth, s_xbins, s_ybins, s_depth, gmass):
+def gas_masses_los(g_xbins, g_ybins, g_depth, s_xbins, s_ybins, s_depth, gmass, kappa=None):
     """
     Compute the number of gas particles in front of each stellar particle along the line of sight using Numba.
     
@@ -112,37 +112,103 @@ def gas_masses_los(g_xbins, g_ybins, g_depth, s_xbins, s_ybins, s_depth, gmass):
     g_xbins, g_ybins, g_depth: 1D arrays of gas particle coordinates
     s_xbins, s_ybins, s_depth: 1D arrays of stellar particle coordinates
     gmass: 1D array of gas particle masses
+    kappa: 1D array of gas opacities. If None, the opacity will be ignored
     
     Returns:
     m_gas: 1D array where m_gas[i] is the total mass of gas particles in front of the i-th stellar particle.
     """
     m_gas = np.zeros(len(s_xbins), dtype=np.float64)
+    avg_kappas = np.zeros(len(s_xbins), dtype=np.float64) if kappa is not None else None
     
     for i in prange(len(s_xbins)):
         count = 0
         mass_sum = 0.0
+        kappa_sum = 0.0
         for j in range(len(g_xbins)):
             if g_xbins[j] == s_xbins[i] and g_ybins[j] == s_ybins[i] and g_depth[j] < s_depth[i]:
                 count += 1
                 mass_sum += gmass[j]
+                if kappa is not None:
+                    kappa_sum += kappa[j]
+                    
         m_gas[i] = mass_sum
+        if kappa is not None:
+            avg_kappas[i] = kappa_sum / count if count > 0 else 0 # compute average kappa
     
-    return m_gas
+    return m_gas, avg_kappas
 
 
-def optical_depth(m_gas, area, kappa=0.2):
+
+
+class los_extinction(object):
     """
-    Determine the optical depth associated with a stellar particle based on the gas mass between it and the observer
+    Determine the effect of exctinction line of sight (LoS) extinction for a given projection
+    The dimming goes like e^-tau, where tau = int (kappa * rho) ds for an optical depth tau, an opacity kappa, a density rho, and a line of sight s
+    Assuming a constant kappa and rho, this just becomes tau = kappa * m_gas / A_pixel (where m_gas in the gas mass between a stellar particle and the observer and A_pixel is the area of a pixel)
+    kappa can then vary based on contributions from electron scattering (kappa_e), Kramer's law (kappa_k) for bound-free/free-free interactions, negative hydrogen ion (kappa_h), and/or molecules such as H20 or CO (kappa_m)
+    For more details on how to calculate kappa, see https://www.astro.princeton.edu/~gk/A403/opac.pdf
+    IMPROTANT: kappa must be in cm^2/g, meaning that m_g is in kg and s is in cm 
+    
     Parameters:
-    m_gas: An array of all gas masses in the line of sight of each stellar particle. IMPORTANT: This must be in grams
-    area:  The area associated associated with the line of sight, which is equal to one pixel area. IMPORTANT: This must be in cm^2
-    kappa: The opacity of the medium along the line of sight, usually derived from properties of gas particles. IMPORTANT: This must be in cm^2/g
+    kappa      :    A custom opacity. If this is not None, all other kappas (kappa_e, k, h, m) will be ignored, and the opacity will be set to this value
+    use_kappa_e:    Do you want to consider opacity from electron scattering? (Important in highly ionized environments). If False, contributions from electron scattering are ignored
+    use_kappa_k:    Do you want to consider opacity from Kramer's law for bound-free/free-free interactions? (Important in partially ionized environments). If False, contributions from Kramer's law are ignored
+    use_kappa_h:    Do you want to consider opacity from the negative hydrogen ion? (Important in solar-like environments). If False, contributions from H^- are ignored are ignored
+    use_kappa_m:    Do you want to consider opacity from molecules? (Important in cold environments). If False, contributions from molecules are ignored
 
-    Returns:
-    The optical depth tau = integral kappa*rho ds = kappa * m_gas / area
+    
     """
-    taus = np.zeros(len(m_gas), dtype=np.float64)
-    taus = np.where(area > 0, (m_gas / area) * kappa, 0.0)
+    def __init__(self, kappa="kappa_e"):
+        self.kappa = kappa
+        
+        
+
+    def get_kappa(self, temps, densities, metals, xH=0.76):
+        temps = np.array(temps, dtype=np.float64)  # Increase floating point precision since this can get raised to high powers
+
+        kappa_e = 0.2 * (1 + xH)
+        kappa_k = 4e25 * metals * (1+xH) * densities * temps**(-3.5)
+        kappa_m = 0.1 * metals  
+        kappa_h = 2.5e-31 * metals * densities**0.5 * np.exp(9 * np.log(temps))
+
+        kappa_arr = None    # initialize
 
         
-    return taus
+        if isinstance(self.kappa, (int, float)):                                      # If the user provides a constant kappa
+            kappa_arr = np.full(metals.shape, self.kappa, dtype=float)
+        elif self.kappa == "kappa_e":                                                 # For electron scattering
+            kappa_arr = np.full(metals.shape, kappa_e, dtype=float)
+        elif self.kappa == "kappa_k":                                                 # For Kramer's opacity for free-free/bound-freee/bound-bound absorption
+            kappa_arr = kappa_k                 
+        elif self.kappa == "kappa_m":                                                 # For molecular opacity
+            kappa_arr = kappa_m
+        elif self.kappa == "kappa_h":                                                 # For negative hydrogen
+            self.kappa = kappa_h
+        elif self.kappa == "kappa_rad":                                               # For the combined radiative opacity (e+k+m+h) as per https://www.astro.princeton.edu/~gk/A403/opac.pdf
+            kappa_h, kappa_e, kappa_k = [np.where(kappa == 0, np.inf, kappa) for kappa in [kappa_h, kappa_e, kappa_k]]   # Replace zero values with np.inf to prevent division by zero in the parallel opacity combination
+            kappa_arr = kappa_m + 1/(1/kappa_h + 1/(kappa_e + kappa_k))   
+        elif self.kappa == "kappa_d":
+            raise ValueError("Line of sight exticntion with dust opacity is currently under development. Please select an alternate opacity source for now.")
+
+        else:
+            raise ValueError("Invalid opacity option. Must be either 'kappa_e' (electron scattering), 'kappa_k' (Kramer's law), 'kappa_m' (molecules), 'kappa_rad' (combined radiation), 'kappa_d' (dust), or a float/int (constant).")
+
+        return kappa_arr
+
+
+    def get_tau(self, kappa, m_gas, area):
+        """
+        Determine the optical depth associated with a stellar particle based on the gas mass between it and the observer
+        Parameters:
+        m_gas: An array of all gas masses in the line of sight of each stellar particle. IMPORTANT: This must be in grams
+        area:  The area associated associated with the line of sight, which is equal to one pixel area. IMPORTANT: This must be in cm^2
+        kappa: The opacity of the medium along the line of sight, usually derived from properties of gas particles. IMPORTANT: This must be in cm^2/g
+
+        Returns:
+        The optical depth tau = integral kappa*rho ds = kappa * m_gas / area
+        """
+        taus = np.zeros(len(m_gas), dtype=np.float64)
+        #kappa = np.full(len(m_gas), 0.02)
+        taus = np.where(area > 0, kappa * (m_gas / area), 0.0)
+
+        return taus
