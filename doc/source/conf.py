5a0e557ed3c43eea68db1b0dd3838ef6d9720c42
Patrick Janulewicz <96801116+pjanul@users.noreply.github.com>
2025-04-29 20:51:55 -0400

    Switch to pydata sphinx theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,6 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
